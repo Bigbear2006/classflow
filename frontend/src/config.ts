1fac9143f0db80aaa7fd6822d8b83e2dc832a8b9
Bigbear2006 <morozm982@gmail.com>
2026-05-03 18:24:13 +0300

    feat(deploy): add docker and nginx

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,3 +1,5 @@
-export const BASE_URL = `${window.location.protocol}//${window.location.hostname}:8000/api/v1/`;
+const PORT = window.location.port ? ':8000': ''
+
+export const BASE_URL = `${window.location.protocol}//${window.location.hostname}${PORT}/api/v1/`;
 
 export const MAX_OWNED_ORGS_COUNT = 1;
